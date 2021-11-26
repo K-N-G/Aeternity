da2847e4681dcfe72e9a1c7cefc00586a06089a7
Nikolay Kovachev <rrr_ood@yahoo.com>
2021-11-26 20:01:46 +0200

    add dependencies and platform setup in Package.swift

--- Package.swift
+++ Package.swift
@@ -5,6 +5,12 @@ import PackageDescription
 
 let package = Package(
     name: "Aeternity",
+    platforms:  [
+        .iOS(.v9),
+        .macOS(.v10_10),
+        .tvOS(.v9),
+        .watchOS(.v2),
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -13,7 +19,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/Flight-School/AnyCodable", from: "0.6.1"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
